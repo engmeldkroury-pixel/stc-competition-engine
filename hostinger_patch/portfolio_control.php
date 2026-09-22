040a8279ecfca5a7b7115a75d4deafa63e7a29d9
engmeldkroury-pixel <eng.m.eldkroury@gmail.com>
2026-09-22 06:14:38 +0300

    Reject recording fills from non-A+ plans

--- hostinger_patch/portfolio_control.php
+++ hostinger_patch/portfolio_control.php
@@ -140,6 +140,9 @@ function stc_validate_stc_plan_position(PDO $pdo, array $body): array {
     if (!is_array($plan) || !is_array($signal)) {
         stc_json(['ok' => false, 'error' => 'source_plan_invalid'], 409);
     }
+    if (($signal['quality_gate_passed'] ?? false) !== true || ($signal['setup_grade'] ?? '') !== 'A_PLUS') {
+        stc_json(['ok' => false, 'error' => 'source_plan_not_high_conviction'], 409);
+    }
 
     $competitionId = (string)($body['competition_id'] ?? '');
     $symbol = (string)($body['symbol'] ?? '');
